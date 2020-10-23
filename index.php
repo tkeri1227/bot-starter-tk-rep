b09ab9ae14d7d424ae1511f9fecd3be54170139a
tkeri1227 <formyselfsunnyday@gmail.com>
2020-10-23 02:30:48 +0000

    add Procfile

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
-echo 'Hello World!';
+// Composerでインストールしたライブラリを一括読み込み
+require_once __DIR__ . '/vendor/autoload.php';
+$inputString = file_get_contents('php://input');
+error_log($inputString);
 
 ?>
